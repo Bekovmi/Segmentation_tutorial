2c62fec37115043e00cf301a0b00de8dce12f90c
Bekovmi <47568254+Bekovmi@users.noreply.github.com>
2019-12-22 18:49:53 +0300

    Update CarSegmentation.py

--- inference/CarSegmentation.py
+++ inference/CarSegmentation.py
@@ -1,16 +1,18 @@
 from inferens import RemoveBackground
 import argparse
 import pandas as pd
-import glob
+import os
 import cv2
-def Car_Segmenatation(path_in, path_out, threshold,path_model,image_size):
+import numpy as np
+def Car_Segmenatation(path_in, path_out, path_model,threshold,image_size):
     mask_predictor = RemoveBackground(path_model,image_size)
 
-    data = pd.DataFrame({"image":glob.glob(path_in)})
-    for image in data.iloc[:,0]:
-       	mask = mask_predictor.predict(image, threshold=0.5)
+    data = pd.DataFrame({"image":os.listdir(path_in)})
+    for image_file in data.iloc[:,0]:
+        image = cv2.resize(cv2.imread(os.path.join(path_in,image_file)), (image_size,image_size))
+       	probability = mask_predictor.predict(image, threshold=0.5)
         image[probability==0]=[255,255,255]
-        cv2.imwrite(os.path.join(path_out,image),image)
+        cv2.imwrite(os.path.join(path_out,image_file),image)
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = 'folders to files')
     parser.add_argument('path_in', type=str, help='dir with image')
@@ -18,6 +20,6 @@ if __name__ == "__main__":
     parser.add_argument('path_model', type = str, help = 'path_model')
     parser.add_argument('threshold', type = float, help = 'threshold')
 
-    parser.add_argument('image_size', type = float, help = 'image_size')
+    parser.add_argument('image_size', type = int, help = 'image_size')
     args = parser.parse_args()
     Car_Segmenatation(args.path_in,args.path_out,args.path_model,args.threshold,args.image_size)
