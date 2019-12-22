3bb77cfe7ad3b56e34374d26323f97c29bf7cf5b
Bekovmi <47568254+Bekovmi@users.noreply.github.com>
2019-12-22 18:50:32 +0300

    Update inferens.py

--- inference/inferens.py
+++ inference/inferens.py
@@ -4,18 +4,19 @@ import torch.nn as nn
 import torch 
 import os
 from transforms import (pre_transforms, post_transforms, Compose)
+from catalyst.dl import utils
 class RemoveBackground:
 	def __init__(self, model_path,image_size):
-		self.transform_pipeline =Compose([pre_transforms(image_size=image_size),post_transforms()])
+		self.augmentation =Compose([pre_transforms(image_size=image_size),post_transforms()])
 		self.m = nn.Sigmoid()
-		self.model = torch.jit.load(model_path).cuda()
+		self.model = utils.load_traced_model(model_path).cuda()
 		device_name = "cuda:0" if torch.cuda.is_available() else "cpu"
 		self.device = torch.device(device_name)
 		self.image_size = image_size
 	def predict(self, image,threshold):
 		augmented = self.augmentation(image=image)
 		inputs = augmented['image']
-		inputs = inputs.to(self.device) 
+		inputs = inputs.unsqueeze(0).to(self.device) 
 		output = self.m(self.model(inputs)[0][0]).cpu().numpy()
 		probability = (output > threshold).astype(np.uint8)
 		return probability
