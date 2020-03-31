ae6124c763e0040954ee281eb54742a55fe4380b
Dmitry Bleklov <47568254+Bekovmi@users.noreply.github.com>
2020-03-31 10:24:39 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,4 @@
+from catalyst.dl import registry, SupervisedRunner as Runner
+from catalyst.contrib.models.cv import segmentation as m
 from .experiment import Experiment
-from catalyst.dl import SupervisedRunner as Runner
+registry.MODELS.add_from_module(m)
